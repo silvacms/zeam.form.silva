74c04e9f90ddd5d77a918f06670251782ae5a662
Sylvain Viollon <thefunny@gmail.com>
2010-05-07 13:37:48 +0200

    Load all widgets by default.

--- src/zeam/form/silva/__init__.py
+++ src/zeam/form/silva/__init__.py
@@ -4,3 +4,6 @@ from zeam.form.silva.form import ZMIForm, ZMIComposedForm
 from zeam.form.composed import *
 from zeam.form.base import *
 from zeam.form.ztk import *
+
+from zeam.form.ztk.fields import initialize_widgets
+initialize_widgets()
