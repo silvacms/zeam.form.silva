1674123944b5e77c3891be70a0ce6e20b751e1b4
Sylvain Viollon <thefunny@gmail.com>
2010-07-01 11:12:26 +0200

    Don't need to call anything to register fields now.

--- src/zeam/form/silva/__init__.py
+++ src/zeam/form/silva/__init__.py
@@ -4,6 +4,3 @@ from zeam.form.silva.form import ZMIForm, ZMIComposedForm, SMIForm, SMIEditForm
 from zeam.form.composed import *
 from zeam.form.base import *
 from zeam.form.ztk import *
-
-from zeam.form.ztk.fields import initialize_widgets
-initialize_widgets()
