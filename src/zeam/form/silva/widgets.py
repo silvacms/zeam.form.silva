8d2d8473fcac8b6eb4c7e1e2446cac751d59ae72
Sylvain Viollon <thefunny@gmail.com>
2010-08-19 14:41:59 +0200

    Add a widget which display an icon.

--- src/zeam/form/silva/widgets.py
+++ src/zeam/form/silva/widgets.py
@@ -1,12 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2010 Infrae. All rights reserved.
+# See also LICENSE.txt
 
 from ZPublisher.HTTPRequest import FileUpload
+from Products.Silva.icon import get_icon_url
 
 from five import grok
 from zope.interface import Interface
 
 from silva.core.conf import schema as silvaschema
 
-from zeam.form.base.markers import INPUT, NO_VALUE
+from zeam.form.base.markers import NO_VALUE
 from zeam.form.base.widgets import WidgetExtractor
 from zeam.form.ztk.fields import SchemaField, SchemaFieldWidget
 from zeam.form.ztk.fields import registerSchemaField
@@ -23,7 +27,7 @@ class FileSchemaField(SchemaField):
 
 class FileWidgetInput(SchemaFieldWidget):
     grok.adapts(FileSchemaField, Interface, Interface)
-    grok.name(str(INPUT))
+    grok.name('input')
 
     def valueToUnicode(self, value):
         return u''
@@ -37,3 +41,12 @@ class FileWidgetExtractor(WidgetExtractor):
         if not isinstance(value, FileUpload) or not value:
             value = NO_VALUE
         return value, None
+
+
+class IconDisplayWidget(SchemaFieldWidget):
+    grok.name('silva.icon')
+
+    def update(self):
+        super(IconDisplayWidget, self).update()
+        content = self.form.getContentData().getContent()
+        self.icon_url = get_icon_url(content, self.request)
