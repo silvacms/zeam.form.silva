1a37d3332113bfd30a7bebd1f252fa6b7c1c4276
Sylvain Viollon <thefunny@gmail.com>
2011-03-09 15:01:43 +0100

    Make so that the widget works not only the first time, but afterwards as well.

--- src/zeam/form/silva/widgets/static/cropping.js
+++ src/zeam/form/silva/widgets/static/cropping.js
@@ -1,40 +1,49 @@
 
 (function($) {
 
-    $(document).ready(function(){
-        $('div.widget-crop').each(function(index) {
-            var widget = $(this);
-            var button = $('a.widget-crop-popup-button', widget);
-            var popup = $('div.widget-crop-popup', widget);
-            var img = $('img.widget-crop-image', popup);
+    var create_cropping_field = function(field) {
+        var button = field.find('a.widget-crop-popup-button');
+        var popup = field.find('div.widget-crop-popup');
+        var img = popup.find('img.widget-crop-image');
 
-            popup.dialog({
-                modal: true,
-                autoOpen: false,
-                buttons: {
-                    Ok: function(){
-                        var c = img.data('crop');
-                        if (c !== undefined) {
-                            $('input', widget).val(
-                                c.x + 'x' + c.y + '-' + c.x2 + 'x' + c.y2);
-                            popup.dialog('close');
-                        }
-                    },
-                    Cancel: function(){ popup.dialog('close'); }
+        popup.dialog({
+            modal: true,
+            autoOpen: false,
+            buttons: {
+                Ok: function(){
+                    var c = img.data('crop');
+                    if (c !== undefined) {
+                        $('input', field).val(
+                            c.x + 'x' + c.y + '-' + c.x2 + 'x' + c.y2);
+                        popup.dialog('close');
+                    }
+                },
+                Cancel: function(){ popup.dialog('close'); }
+            }
+        });
+
+        button.bind('click', function(event){
+            img.Jcrop({
+                onSelect: function(c){
+                    img.data('crop', c);
+                },
+                onChange: function(c){
+                    img.data('crop', c);
                 }
             });
+            popup.dialog('open');
+        });
+    };
 
-            button.bind('click', function(event){
-                img.Jcrop({
-                    onSelect: function(c){
-                        img.data('crop', c);
-                    },
-                    onChange: function(c){
-                        img.data('crop', c);
-                    }
-                });
-                popup.dialog('open');
-            });
+    $('form').live('load-smiform', function() {
+        $(this).find('div.widget-crop').each(function(index, field){
+            create_cropping_field($(field));
+        });
+    });
+
+    $(document).ready(function(){
+        $('div.widget-crop').each(function(index, field) {
+            create_cropping_field($(field));
         });
     });
 
