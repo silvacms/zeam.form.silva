729685cd8063bbbce93199d728244dbf8af54674
Sylvain Viollon <thefunny@gmail.com>
2011-03-10 18:46:41 +0100

    Fix template. Focus management on SMI forms moved to silva.ui.

--- src/zeam/form/silva/form/static/smi.js
+++ src/zeam/form/silva/form/static/smi.js
@@ -71,44 +71,7 @@
         container.children('.form-error-detail').remove();
     };
 
-    /**
-     * Field focus management.
-     */
-    var focus_form_field = function(field) {
-        var section = field.closest('.form-section');
-        if (section.hasClass('form-focus')) {
-            return;
-        };
-        var form = section.closest('form');
-        form.find('.form-section').removeClass('form-focus');
-        section.addClass('form-focus');
-        section.find('.field:first').focus();
-    };
-
-    $('.form-section').live('focusin', function() {
-        focus_form_field($(this));
-    });
-
-    $('.form-section').live('click', function(){
-        focus_form_field($(this));
-    });
-
-    var focus_first_form_field = function(form) {
-        var field = form.find('.field-error:first').find('.field:first');
-        if (field.length) {
-            // First error field
-            focus_form_field(field);
-        } else {
-            // Focus first required field otherwise
-            field = form.find('.field-required:first');
-            focus_form_field(field);
-        };
-    };
-
     var bootstrap_form = function(form) {
-        // Focus
-        focus_first_form_field(form);
-
         // Select all
         form.find('.zeam-select-all').bind('change', function() {
             var select = $(this);
@@ -127,7 +90,6 @@
     /**
      * Popup form.
      */
-
     var Popup = function(popup, url) {
         this.popup = popup;
         this.url = url;
