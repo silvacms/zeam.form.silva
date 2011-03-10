972327c720e37a1a00556514a6133a74d359ad35
Sylvain Viollon <thefunny@gmail.com>
2011-03-10 15:16:53 +0100

    Validation on composed forms.

--- src/zeam/form/silva/validation.py
+++ src/zeam/form/silva/validation.py
@@ -8,32 +8,60 @@ from five import grok
 from silva.ui.rest.base import UIREST
 
 from zeam.form.base.interfaces import IFormCanvas
+from zeam.form.composed.interfaces import ISubFormGroup
 from zeam.form.base.widgets import getWidgetExtractor
 from zeam.form.silva.utils import convert_request_form_to_unicode
+
+
+def is_prefix(prefix, value):
+    return (value.startswith(prefix) and
+            (len(value) == len(prefix) or
+             value[len(prefix)] == '.'))
 
 
 class RESTValidatorForm(UIREST):
     grok.name('zeam.form.silva.validate')
     grok.context(IFormCanvas)
 
+    def get_form(self, prefix):
+
+        def getter(form):
+            if ISubFormGroup.providedBy(form):
+                for subform in form.allSubforms:
+                    if is_prefix(subform.prefix, prefix):
+                        if subform.prefix == prefix:
+                            return subform
+                        return getter(subform)
+            elif form.prefix == prefix:
+                return form
+            raise KeyError(prefix)
+
+        return getter(self.context)
+
     def validate(self):
         convert_request_form_to_unicode(self.request.form)
-        name = self.request.form['prefix.field']
-
         info = {'success': True}
+        fieldname = self.request.form['prefix.field']
 
-        # Look for extractor, extract and validate value.
-        for field in self.context.fields:
-            extractor = getWidgetExtractor(field, self.context, self.request)
-            if extractor is not None:
-                if extractor.identifier == name:
-                    value, error = extractor.extract()
-                    if error is None:
-                        error = field.validate(value, self.context.context)
-                    if error is not None:
-                        info['success'] = False
-                        info['error'] = self.translate(error)
-                    break
+        try:
+            form = self.get_form(self.request.form['prefix.form'])
+        except KeyError:
+            info['success'] = False
+        else:
+            # Look for extractor, extract and validate value.
+            for field in form.fields:
+                extractor = getWidgetExtractor(field, form, self.request)
+                if extractor is not None:
+                    if extractor.identifier == fieldname:
+                        value, error = extractor.extract()
+                        if error is None:
+                            error = field.validate(value, self.context.context)
+                        if error is not None:
+                            info['success'] = False
+                            info['error'] = self.translate(error)
+                        break
+            else:
+                info['success'] = False
         return self.json_response(info)
 
     def POST(self):
