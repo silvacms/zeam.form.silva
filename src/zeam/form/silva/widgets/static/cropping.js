e87737f97a552af2ba0df4641e4e87af7cecc118
Sylvain Viollon <thefunny@gmail.com>
2011-07-01 13:05:25 +0200

    Fix leaking on dialog.

--- src/zeam/form/silva/widgets/static/cropping.js
+++ src/zeam/form/silva/widgets/static/cropping.js
@@ -1,85 +1,98 @@
 
+
 (function($) {
 
-    var create_cropping_field = function(field) {
-        var button = field.find('a.widget-crop-popup-button');
-        var popup = field.find('div.widget-crop-popup');
-        var img = popup.find('img.widget-crop-image');
-        var cbx = field.find('input.crop-proportional');
+    var create_cropping_field = function() {
+        var $field = $(this);
+        var $input = $field.find('input');
+        var $opener = $field.find('a.widget-crop-popup-button');
+        var $template = $field.find('div.widget-crop-popup');
+        var required = $input.hasClass('required');
 
         var width = $(window).width();
         var height = $(window).height();
 
-        var imageWidth = width - width * 0.2;
-        var imageHeight = height - height * 0.3;
+        var imageWidth = 0.8 * width;
+        var imageHeight = 0.7 * height;
 
-        var popupWidth = width - width * 0.1;
-        var popupHeight = height - height * 0.1;
+        var popupWidth = 0.9 * width;
+        var popupHeight = 0.9 * height;
 
-        var api = null;
+        $opener.bind('click', function(event){
+            var $popup = $template.clone();
+            var $img = $popup.find('img.widget-crop-image');
+            var $proportional = $popup.find('input.crop-proportional');
+            var crop = undefined;
+            var jCrop = undefined;
 
-        cbx.change(function(){
-            if (api !== null) {
-                if (!!this.checked) {
-                    api.setOptions({aspectRatio: img.width() / img.height()});
-                } else {
-                    api.setOptions({aspectRatio: 0});
-                }
-            }
-        });
-
-        popup.dialog({
-            modal: true,
-            autoOpen: false,
-            width: popupWidth,
-            height: popupHeight,
-            open: function() {
-                if (api === null) {
-                    api = $.Jcrop(img, {
-                        boxWidth: imageWidth,
-                        boxHeight: imageHeight,
-                        onSelect: function(c){
-                            img.data('crop', c);
-                        },
-                        onChange: function(c){
-                            img.data('crop', c);
-                        }
-                    });
-                }
-                var value = $('input', field).val();
-                var select = value.replace(
-                        /^\s*(\d+)x(\d+)-(\d+)x(\d+)\s*$/,
-                        '$1,$2,$3,$4').split(',');
-                api.setSelect(select);
-            },
-            buttons: {
+            // Define popup actions
+            var actions = {
                 Ok: function(){
-                    var c = img.data('crop');
-                    if (c !== undefined) {
-                        $('input', field).val(
-                            c.x + 'x' + c.y + '-' + c.x2 + 'x' + c.y2);
-                        popup.dialog('close');
+                    if (crop !== undefined) {
+                        $input.val(
+                            crop.x + 'x' + crop.y + '-' + crop.x2 + 'x' + crop.y2);
+                        $popup.dialog('close');
                     }
                 },
-                Cancel: function(){ popup.dialog('close'); }
-            }
-        });
+                Clear: function() {
+                    $input.val('');
+                    $popup.dialog('close');
+                },
+                Cancel: function() {
+                    $popup.dialog('close');
+                }
+            };
+            if (required) {
+                delete actions['Clear'];
+            };
 
-        button.bind('click', function(event){
-            popup.dialog('open');
+            // Bind proportional option
+            $proportional.change(function(){
+                if (jCrop !== null) {
+                    if (!!this.checked) {
+                        jCrop.setOptions({aspectRatio: $img.width() / $img.height()});
+                    } else {
+                        jCrop.setOptions({aspectRatio: 0});
+                    };
+                };
+            });
+
+            // Cleanup everything when the popup is closed
+            $popup.bind('dialogclose', function() {
+                $popup.remove();
+            });
+            $popup.dialog({
+                modal: true,
+                autoOpen: true,
+                width: popupWidth,
+                height: popupHeight,
+                open: function() {
+                    jCrop = $.Jcrop($img, {
+                        boxWidth: imageWidth,
+                        boxHeight: imageHeight,
+                        onSelect: function(value){
+                            crop = value;
+                        },
+                        onChange: function(value){
+                            crop = value;
+                        }
+                    });
+                    var select = $input.val().replace(
+                            /^\s*(\d+)x(\d+)-(\d+)x(\d+)\s*$/,
+                        '$1,$2,$3,$4').split(',');
+                    jCrop.setSelect(select);
+                },
+                buttons: actions
+            });
         });
     };
 
     $('form').live('load-smiform', function() {
-        $(this).find('div.widget-crop').each(function(index, field){
-            create_cropping_field($(field));
-        });
+        $(this).find('div.widget-crop').each(create_cropping_field);
     });
 
     $(document).ready(function(){
-        $('div.widget-crop').each(function(index, field) {
-            create_cropping_field($(field));
-        });
+        $('div.widget-crop').each(create_cropping_field);
     });
 
 })(jQuery);
