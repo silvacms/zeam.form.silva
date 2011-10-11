5233d939237f95489a70cd8ed20b16e4acea834f
Sylvain Viollon <thefunny@gmail.com>
2011-10-11 16:22:33 +0200

    Add an import.

--- src/zeam/form/silva/__init__.py
+++ src/zeam/form/silva/__init__.py
@@ -12,6 +12,7 @@ from zeam.form.silva.form import SMIComposedForm, SMISubForm, SMISubFormGroup
 from zeam.form.silva.form import SMISubTableForm
 from zeam.form.silva.form import SMIViewletForm, PublicViewletForm
 from zeam.form.silva.form import SMIContentProviderForm, PublicContentProviderForm
+from zeam.form.silva.form import SilvaDataManager
 from zeam.form.silva.form import PublicForm
 from zeam.form.silva.rest import RESTPopupForm, RESTKupuEditProperties
 from zeam.form.silva.actions import EditAction, CancelAction, PopupAction
