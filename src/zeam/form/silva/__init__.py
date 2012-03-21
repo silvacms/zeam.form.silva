9738fc95649cd4893fa054ed4002aae40409df74
Sylvain Viollon <thefunny@gmail.com>
2012-03-21 16:14:54 +0100

    Update imports.

--- src/zeam/form/silva/__init__.py
+++ src/zeam/form/silva/__init__.py
@@ -9,7 +9,8 @@ from zeam.form.table import *
 from zeam.form.silva.form.zmi import ZMIForm, ZMIComposedForm, ZMISubForm
 
 from zeam.form.silva.form.smi import SMIForm, SMIAddForm, SMIEditForm
-from zeam.form.silva.form.smi import SMIComposedForm, SMISubForm, SMISubFormGroup
+from zeam.form.silva.form.smi import SMIComposedForm
+from zeam.form.silva.form.smi import SMISubForm, SMISubFormGroup
 from zeam.form.silva.form.smi import SMITableForm, SMISubTableForm
 from zeam.form.silva.form.smi import SMIFormPortlets
 from zeam.form.silva.form.public import PublicViewletForm
@@ -17,8 +18,12 @@ from zeam.form.silva.form.public import PublicContentProviderForm
 from zeam.form.silva.form.public import PublicForm
 from zeam.form.silva.form.popup import RESTPopupForm, PopupForm
 
-from zeam.form.silva.datamanager import SilvaDataManager, FieldValueDataManagerFactory
+from zeam.form.silva.datamanager import SilvaDataManager
+from zeam.form.silva.datamanager import FieldValueDataManagerFactory
 from zeam.form.silva.actions import ExtractedDecoratedAction, LinkAction
 from zeam.form.silva.actions import EditAction, CancelAction, PopupAction
 
 from zeam.form.silva.interfaces import IDefaultAction
+from zeam.form.silva.interfaces import IRESTCloseOnSuccessAction
+from zeam.form.silva.interfaces import IRESTExtraPayloadProvider
+
